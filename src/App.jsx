12588bad255d2ee6375919aac3c4e967bb8b5257
Hazem-Elabd <elabdhazem4@gmail.com>
2026-07-04 12:17:40 +0300

    fix routing for production

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import "./App.css";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.jsx";
+
 import { WishlistProvider } from "./Context/WishlistContext";
 import { CartProvider } from "./Context/CartContext.jsx";
 import { SearchProvider } from "./Context/SearchContext.jsx";
